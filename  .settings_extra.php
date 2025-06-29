c66067420dece3999eb719f56548cac78689acb6
Andrey Luamin <lav.perm@mail.ru>
2025-06-29 14:57:22 +0300

    Конфликты из за ручной правки

---
+++  .settings_extra.php
@@ -0,0 +1,25 @@
+<?php
+return array (
+	'exception_handling' =>
+		array (
+			'value' =>
+				array (
+					'debug' => true,
+					'handled_errors_types' => E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE & ~E_DEPRECATED,
+					'exception_errors_types' => E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE & ~E_DEPRECATED,
+					'ignore_silence' => false,
+					'assertion_throws_exception' => true,
+					'assertion_error_type' => 256,
+					'log' => array (
+						'class_name' => 'App\Debug\FileExceptionHandlerMyLogOtus::class',
+						'required_file' => 'local/App/Debug/FileExceptionHandlerMyLogOtus.php',
+						'settings' =>
+							array (
+								'file' => 'Otus/logs/CustomExceptionLogs.log',
+								'log_size' => 1000000,
+								),
+			'readonly' => false,
+									)
+					)
+			)
+);
