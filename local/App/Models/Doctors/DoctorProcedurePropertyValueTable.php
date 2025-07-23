06525b1d9df5a466651e901db72a7e500f043ce7
@Ndron <lav.perm@mail.ru>
2025-07-23 15:36:26 +0500

    Урок доктора

---
+++ local/App/Models/Doctors/DoctorProcedurePropertyValueTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Doctors;
+
+use Models\AbstractIblockPropertyValuesTable;
+
+class DoctorProcedurePropertyValueTable extends AbstractIblockPropertyValuesTable
+{
+	const IBLOCK_ID = 21;   //Процедуры
+}
---
+++ local/App/Models/Doctors/DoctorSpecializationPropertyValuesTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Doctors;
+
+use Models\AbstractIblockPropertyValuesTable;
+
+class DoctorSpecializationPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+	const IBLOCK_ID = 22;  //Специлизация 22
+}
