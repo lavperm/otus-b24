ad6b1ad1cde72b432247765fe6f8ad473a3a7ce1
@Ndron <lav.perm@mail.ru>
2025-09-07 16:46:14 +0500

    правки

--- Otus/currencies.php
+++ Otus/currencies.php
@@ -1,1 +1,23 @@
 <?php
+global $APPLICATION;
+
+require ($_SERVER["DOCUMENT_ROOT"] . '/bitrix/header.php');
+$APPLICATION->SetTitle('Курс волют ДЗ №5');
+
+?>
+
+<?php
+$APPLICATION->IncludeComponent(
+	"castom:currencies",
+	".default",
+	[
+
+		'CURRENCY' => 'RUB',
+
+	],
+	false
+);
+?>
+
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+
--- local/components/castom/currencies/class.php
+++ local/components/castom/currencies/class.php
@@ -1,1 +1,27 @@
 <?php
+use Bitrix\Main\Loader;
+use Bitrix\Main\Application;
+use Bitrix\Iblock;
+use Models\Currencies\CurrenciesTable;
+
+class CurrenciesComponent extends \CBitrixComponent
+{
+	protected $request;
+
+	public function executeComponent()
+	{
+		$this->$request = Application::getInstance()->getContext()->getRequest();
+
+		$res= CurrenciesTable::query()
+			->addSelect("CURRENCY")
+			->addSelect("AMOUNT")
+			->addFilter('=CURRENCY' ,$this->arParams['CURRENCY'])
+			->fetch();
+
+		$this->arParams['VALUE_CURRENCY'] = $res['AMOUNT'];
+		$this->includeComponentTemplate();
+	}
+
+
+
+}
